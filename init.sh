303587aa7827505e76e3db66560bb5c17cd033d3
defacer <saibalaji@outlook.com>
2018-07-18 21:19:24 +0530

    1. Spinning up a new Kubernetes pod to create a Jenkins slave instance 2. Creating a Terraform Plan to provision infrastructure for running a 3-Tier application 3. Invoking Chef for configuring Tomcat apache and MySQL
    1. Spinning up a new Kubernetes pod to create a Jenkins slave instance
    2. Creating a Terraform Plan to provision infrastructure for running a 3-Tier application
    3. Invoking Chef for configuring Tomcat apache and MySQL

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 echo "1.Spinning up a new Kubernetes pod to create a Jenkins slave instance
 2. Initializing Terraform to provision infrastructure for running a 3-Tier application
-3. Invoking Chef for configuring Tomcat apache and MySQL"
+3. Invoking Chef for configuring Tomcat apache and MySQL!"
 terraform -init
